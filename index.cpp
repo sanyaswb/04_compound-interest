ac6d5fca7d71d478671b0a792ca179babd3305c6
John Doe <SlySpellar@gmail.com>
2022-10-05 00:19:42 +0300

    removed solution

--- index.cpp
+++ index.cpp
@@ -3,5 +3,5 @@
 #include <cmath>
 
 long compoundInterest(long startDeposit, double annualRate, int annualAmount, int interestPeriod) {
-	return round(startDeposit * pow(1 + annualRate / annualAmount, annualAmount * interestPeriod));
+	return 0;
 }
